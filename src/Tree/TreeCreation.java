dfcc6eda7978f312b216dbb94459a39f400e53a1
Mahesh Kumar <maheshkumarnit96@gmail.com>
2022-12-27 11:55:05 +0530

    fix:added tree traversal method (preorder, inorder, and postorder)

--- src/Tree/TreeCreation.java
+++ src/Tree/TreeCreation.java
@@ -7,22 +7,56 @@ public class TreeCreation {
         int data;
         Scanner scn = new Scanner(System.in);
         Node root = null;
-        System.out.print("Enter data");
+//        System.out.print("Enter data");
         data = scn.nextInt();
         if(data == -1){
             return null;
         }
         root = new Node(data);
-        System.out.print("Enter left child of"+root);
+        System.out.print("Enter left child of "+root.data+" ");
         root.left = create();
-        System.out.print("Enter right child of"+root);
+        System.out.print("Enter right child of "+root.data+" ");
         root.right = create();
 
         return root;
     }
 
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    public static void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        inorder(root.left);
+        inorder(root.right);
+    }
+    public static void postOrder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        inorder(root.right);
+        System.out.print(root.data+" ");
+    }
+
     public static void main(String[] args) {
+
         Node root = create();
+        System.out.println("Preorder traversal of given tree is: ");
+        preOrder(root);
+        System.out.println();
+        System.out.println("Inorder traversal of given tree is: ");
+        inorder(root);
+        System.out.println();
+        System.out.println("Postorder traversal of given tree is: ");
+        postOrder(root);
     }
 }
 
