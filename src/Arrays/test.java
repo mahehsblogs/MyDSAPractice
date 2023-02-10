b1066f4a55d5340d23a378e8e8c01777cb469ef5
Mahesh Kumar <maheshkumarnit96@gmail.com>
2023-02-10 22:40:57 +0530

    write some test code for practices

--- src/Arrays/test.java
+++ src/Arrays/test.java
@@ -1,18 +1,45 @@
 package Arrays;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.PriorityQueue;
 
 public class test {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i< n; i++){
-            arr[i] = scn.nextInt();
+    public static int minSizeSubarray(int[] A, int[] B, long S) {
+        int l = 0, r = 0;
+        long prod = 0;
+        int minSize = Integer.MAX_VALUE;
+        while (l < A.length) {
+            while (r < B.length && (prod = A[l] + B[r]) < S) {
+                r++;
+            }
+            if (r < B.length && prod >= S) {
+                minSize = Math.min(minSize, (l + 1) + (r + 1));
+            }
+            l++;
+            if (r == B.length) {
+                break;
+            }
         }
+        return (minSize == Integer.MAX_VALUE) ? -1 : minSize;
+    }
 
-        for(int i = n-1; i >=0; i--){
-            System.out.print(arr[i]+" ");
+    public static void main (String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0){
+            int n = scn.nextInt();
+            int m = scn.nextInt();
+            int[] A = new int[n];
+            int[] B = new int[m];
+            for(int i = 0; i < n; i++){
+                A[i] = scn.nextInt();
+            }
+            for(int i = 0; i < m; i++){
+                B[i] = scn.nextInt();
+            }
+            long S = scn.nextLong();
+
+            System.out.println(minSizeSubarray(A, B, S));
         }
     }
 }
