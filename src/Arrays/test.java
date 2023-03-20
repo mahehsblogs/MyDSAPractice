b4e22a0631e61bf7ca00a8e6586722c79edf25b0
Mahesh Kumar <maheshkumarnit96@gmail.com>
2023-03-20 20:17:52 +0530

    fix:fixes the existing code

--- src/Arrays/test.java
+++ src/Arrays/test.java
@@ -4,42 +4,21 @@ import java.util.*;
 import java.util.PriorityQueue;
 
 public class test {
-    public static int minSizeSubarray(int[] A, int[] B, long S) {
-        int l = 0, r = 0;
-        long prod = 0;
-        int minSize = Integer.MAX_VALUE;
-        while (l < A.length) {
-            while (r < B.length && (prod = A[l] + B[r]) < S) {
-                r++;
-            }
-            if (r < B.length && prod >= S) {
-                minSize = Math.min(minSize, (l + 1) + (r + 1));
-            }
-            l++;
-            if (r == B.length) {
-                break;
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int l = sc.nextInt();
+        String[] strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            strings[i] = sc.next();
         }
-        return (minSize == Integer.MAX_VALUE) ? -1 : minSize;
-    }
+        sc.close();
 
-    public static void main (String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while(t-- > 0){
-            int n = scn.nextInt();
-            int m = scn.nextInt();
-            int[] A = new int[n];
-            int[] B = new int[m];
-            for(int i = 0; i < n; i++){
-                A[i] = scn.nextInt();
-            }
-            for(int i = 0; i < m; i++){
-                B[i] = scn.nextInt();
-            }
-            long S = scn.nextLong();
-
-            System.out.println(minSizeSubarray(A, B, S));
+        Arrays.sort(strings);
+        StringBuilder sb = new StringBuilder();
+        for (String s : strings) {
+            sb.append(s);
         }
+        System.out.println(sb.toString());
     }
 }
--- src/HashMap/DuplicateElement.java
+++ src/HashMap/DuplicateElement.java
@@ -7,6 +7,15 @@ public class DuplicateElement {
     private static int duplicateElement(int[] arr, int n){
         HashMap<Integer, Integer> mp = new HashMap<>();
         for (int i = 0; i < n; i++){
+            if(mp.containsKey(arr[i])){
+                mp.put(arr[i], mp.get(arr[i])+1);
+            }
+            mp.put(arr[i], 1);
+        }
+        for (int i = 0; i < n; i++) {
+            if(mp.get(arr[i]) > 1){
+                return arr[i];
+            }
         }
         return -1;
     }
@@ -17,5 +26,6 @@ public class DuplicateElement {
         for (int i = 0; i < n; i++){
             arr[i] = scn.nextInt();
         }
+        System.out.println(duplicateElement(arr, n));
     }
 }
